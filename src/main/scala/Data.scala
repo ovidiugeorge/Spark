41c0ed6ce2bbc7a7da7a43913ae90822b4edeb1f
ovidiugeorge <ovidiu_george_paiu@yahoo.ro>
2019-03-18 12:06:22 +0200

    finish

--- src/main/scala/Data.scala
+++ src/main/scala/Data.scala
@@ -1,66 +1,64 @@
+%spark
 
-import java.text.SimpleDateFormat
+import spark.implicits._
 import java.util.Calendar
-
-import org.apache.log4j._
-import org.apache.spark._
-
 import scala.collection.mutable.ArrayBuffer
+import java.text.SimpleDateFormat
 
-case class Department(id_product: Int, start_date: String, end_date: String)
+case class pattern (userId : Int , start_date: String, end_date: String )
 
-object Data {
-
-  def splitDate(start: String, end: String): Array[String] = {
-
-    val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-    val result = ArrayBuffer(start)
-
-    val start_date = Calendar.getInstance()
-    val final_date = Calendar.getInstance()
-
-    final_date.setTime(sdf.parse(end))
-    start_date.setTime(sdf.parse(start))
-
-    start_date.add(Calendar.DATE, 1)
-    start_date.set(Calendar.SECOND, 0)
-    start_date.set(Calendar.HOUR_OF_DAY, 0)
-    start_date.set(Calendar.MINUTE, 0)
-
-
-
-    while(start_date.get(Calendar.DAY_OF_MONTH)<final_date.get(Calendar.DAY_OF_MONTH))
-    {
-
-      result += sdf.format(start_date.getTime());
-      start_date.add(Calendar.DAY_OF_MONTH, 1)
-
-    }
-
-    result += sdf.format(start_date.getTime());
-    result += sdf.format(final_date.getTime())
-
-    //result
-    result.toArray
-  }
-
-  def main(args: Array[String]): Unit = {
-    val firstProd = Array(1, "2018-01-01 15:15:20", "2018-01-03 00:00:00")
-    val secondProd = Array(2, "2018-03-01 15:25:20", "2018-02-014 00:00:00")
-    val input = Array(firstProd , secondProd)
-
-
-    val rez = input.map (line => Array(line ++  Array("2018-03-01 15:25:20", "2018-02-014 00:00:00") ) )
-    rez.map (line => println(line.deep.mkString("\n")))
-    //Array(splitDate("2018-03-01 15:25:20", "2018-03-03 00:00:00")).map(line => println(line.deep.mkString("\n")))
-    //val sc = new SparkContext("local[*]","Data")
-    //val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-   // val dataFrame = input.toDF
-
-
-
-
-  }
-
+def generateInterval(arr: Array[Any]): Array[Array[Any]] = {
+  val id = arr.head
+  val right = arr.tail.tail
+  val left = arr.tail.dropRight(1)
+  val res = (left zip right)
+  res.map(line => Array(id, line._1,line._2))
 
 }
+
+def splitDate(idprod:String, start: String, end: String): Array[Any] = {
+
+  val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+  val result = ArrayBuffer(idprod,start)
+
+  val start_date = Calendar.getInstance()
+  val final_date = Calendar.getInstance()
+
+  final_date.setTime(sdf.parse(end))
+  start_date.setTime(sdf.parse(start))
+
+  start_date.add(Calendar.DATE, 1)
+  start_date.set(Calendar.SECOND, 0)
+  start_date.set(Calendar.HOUR_OF_DAY, 0)
+  start_date.set(Calendar.MINUTE, 0)
+
+  while(start_date.get(Calendar.DAY_OF_MONTH)<final_date.get(Calendar.DAY_OF_MONTH)) {
+    result += sdf.format(start_date.getTime())
+    start_date.add(Calendar.DAY_OF_MONTH, 1)
+  }
+
+  if(start_date.get(Calendar.DAY_OF_MONTH)==final_date.get(Calendar.DAY_OF_MONTH))
+    result += sdf.format(start_date.getTime())
+
+  //not add same date in list
+  if(start_date.compareTo(final_date)!=0)
+    result += sdf.format(final_date.getTime())
+
+  result.toArray
+}
+
+
+val firstProd = Array(1, "2018-01-01 15:15:20", "2018-01-03 00:00:01")
+val secondProd = Array(2, "2018-03-01 15:25:20", "2018-02-01 18:00:00")
+val input = Array(firstProd , secondProd)
+
+val splitIntoIntervales = input.map (line => splitDate(line(0).toString,line(1).toString, line(2).toString))
+val generateIntervales = splitIntoIntervales.flatMap(line => generateInterval(line))
+//val test = generateIntervales.map(line => line.productIterator.toList)
+
+val  prodFrame = sc.parallelize(generateIntervales.map(r1 => pattern(r1(0).toString.toInt,r1(1).toString,r1(2).toString))).toDF()
+prodFrame.show()
+
+
+
+
---
+++ src/main/scala/FlatMap_Zip.scala
@@ -0,0 +1,24 @@
+object FlatMap_Zip {
+
+
+  def generateInterval(arr: Array[Any]): Array[Any] = {
+
+    //id
+    val id = arr.head
+
+    val right = arr.tail.tail
+    val left = arr.tail.dropRight(1)
+    val res = (left zip right)
+    res.map(line => (id, line._1,line._2))
+  }
+
+  def main(args: Array[String]): Unit = {
+
+
+    val firstProd = Array(Array(1, "1", "2", "3", "4", "5"), Array(3, "11", "21", "31", "41", "51"))
+    val res = firstProd.flatMap(line => generateInterval(line))
+    val t = (1,"43",5).productIterator.toArray
+    t.
+  }
+
+}
